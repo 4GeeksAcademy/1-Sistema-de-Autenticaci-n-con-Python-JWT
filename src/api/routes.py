b5c3f9a0df1d9950cd11e27c2928279d0ee4d7ce
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-04-18 16:26:05 +0000

    terminando la ruta /protected

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,32 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Primera ruta para el login
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # remplazar con la logica para consultar la base de datos 
+    if email != "loco@loco" or password != "loco":
+        response_body['message'] = "error con el Email o con la Password"
+        return response_body, 401  
+    access_token = create_access_token(identity=[email, 'Funciona']) # aqui va  todos los datos que quiero que me devuelta
+    # response_body['access_token'] = "access Token"
+    # response_body['message'] = "Logeado con existo y con el token"
+    return access_token, 200
+
+
+# Segunda ruta para la pagina privada 
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    print(current_user)
+    return jsonify(logged_in_as=current_user), 200
--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -39,6 +40,11 @@ setup_commands(app)
 
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
+
+# estas lineas es para confi. la extencion de Flask-JWT-Extended 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY") # esta clave sera la raiz de todos los token   # ("JWT_SECRET_KEY") esto sale de la variable del .env
+jwt = JWTManager(app)
+
 
 # Handle/serialize errors like a JSON object
 
