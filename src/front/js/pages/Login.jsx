9d543ecb8d513fad2d2336b4e072dd910033f423
Carlos Betancourt <146739071+DonBeta@users.noreply.github.com>
2024-04-23 19:25:51 +0000

    termine

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -41,7 +41,7 @@ export const Login = () => {
         store.isLogin ? <Navigate to='/dashboard' /> :
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-6">
+                    <div className="col-md-5">
                         <div>
                             <h1 className="text-center">Log in</h1>
                             <form>
